b124132a28191a7da98f1f42704c2477ea573ea0
Tushit Garg <tushit.23@gmail.com>
2020-05-17 13:41:23 +0530

    Find All Anagrams in a String

---
+++ Week 3: May 15th–May 21st/Find All Anagrams in a String.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> index;
+        char ch;
+        int ls,lp;
+        int freqtemp[26],freqp[26];
+        memset(&freqp,0,sizeof(freqp));
+        ls = s.size();
+        lp = p.size();
+        for(int j=0;j<lp;j++)
+            {
+                ch = p[j];
+                freqp[ch-'a']++;
+            }
+        for(int i=0;i<=ls-lp;i++)
+        {
+            memset(&freqtemp,0,sizeof(freqtemp));
+            for(int j=0;j<lp;j++)
+            {
+                ch=s[i+j];
+                freqtemp[ch-'a']++;
+            }
+            int flag=1;
+            for(int j=0;j<26;j++)
+            {
+                if(freqp[j]!=freqtemp[j])
+                {
+                    flag=0;
+                    break;
+                }
+            }
+            if(flag)
+                index.push_back(i);
+        }
+        return index;
+        
+    }
+};
